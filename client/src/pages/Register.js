c69a1d574e4bfcecaffe8479d261ee07822972bb
Cheikhna-sakho <cheikh.bsakho@gmail.com>
2022-07-15 01:58:54 +0200

    clean client

--- client/src/pages/Register.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import RegisterForm from '../components/auth/Register'
-import Layout from '../template/Layout'
-
-const Register = () => {
-  return <Layout content={<RegisterForm/>} />
-}
-
-export default Register
--- client/src/pages/Welcom.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Layout from '../template/Layout'
-
-const Welcom = () => {
-  return <Layout content={<div>Coucou</div>}/>
-}
-
-export default Welcom
